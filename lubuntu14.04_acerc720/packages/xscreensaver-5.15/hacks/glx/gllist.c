fe89d171f0f2e2acb6ff940ec552f81a2ed6dc8c
Hugh Greenberg <hugegreenbug@gmail.com>
2015-02-28 20:59:37 -0700

    Cleanups Added my name to my patches

--- lubuntu14.04_acerc720/packages/xscreensaver-5.15/hacks/glx/gllist.c
+++
@@ -1,9 +0,0 @@
-#include "gllist.h"
-
-void renderList(const struct gllist *list, int wire_p){
-	for(;list!=NULL;list=list->next){
-		glInterleavedArrays(list->format,0,list->data);
-		glDrawArrays((wire_p ? GL_LINE_LOOP : list->primitive),
-                             0,list->points);
-	}
-}
